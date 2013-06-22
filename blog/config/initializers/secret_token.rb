341e17a70d069838fa9700fae5b8b22e4bf0e12c
shiraji <isogai.shiraji@gmail.com>
2013-06-23 02:34:58 +0900

    add 2 exercises

---
+++ blog/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '5ed4c5e18f139a9d2a17b4683c22be7a8ccbb6fa1d484df2180fef9063b14abce936174e76900d2f74b5d7be219d77a7cecebbb3abe2ebbaa3b340bdacde1721'
---
+++ myapp/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Myapp2::Application.config.secret_token = 'a85fdada5b7041e658ec2703738939a4662e2dcc26b3422d653d86592ead5a621263d0b1618bbc2349e25e8de0909d24ccbf2a5254961dc2c9fe334771fe5a07'
